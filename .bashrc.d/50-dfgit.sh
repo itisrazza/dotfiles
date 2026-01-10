f833ba3836a7bfd408ce6c479d2a1639a230001e
Raresh Nistor <raresh@nistor.email>
2026-01-10 21:27:13 +1300

    dfgit: change path

--- .bashrc.d/50-dfgit.sh
+++ .bashrc.d/50-dfgit.sh
@@ -1,4 +1,4 @@
 # dotfiles management
 
-export DOTFILES_DIR="$HOME/src/rz/dotfiles"
+export DOTFILES_DIR="$HOME/src/itisrazza/dotfiles"
 alias dfgit="git --git-dir=\"${DOTFILES_DIR}\" --work-tree=\"${HOME}\""
