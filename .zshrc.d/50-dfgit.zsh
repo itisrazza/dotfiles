4b5dd3666a3d60ae03c88e96a7ea62193049b518
Raresh Nistor <raresh@nistor.email>
2026-06-04 18:08:22 +1200

    change df*git paths to include .git at the end

--- .zshrc.d/50-dfgit.zsh
+++ .zshrc.d/50-dfgit.zsh
@@ -1,8 +1,8 @@
 # dotfiles management
 
-export DOTFILES_COMMON="$HOME/src/itisrazza/dotfiles/common"
-export DOTFILES_PLATFORM="$HOME/src/itisrazza/dotfiles/platform"
-export DOTFILES_SECRET="$HOME/src/itisrazza/dotfiles/secret"
+export DOTFILES_COMMON="$HOME/src/itisrazza/dotfiles/common.git"
+export DOTFILES_PLATFORM="$HOME/src/itisrazza/dotfiles/platform.git"
+export DOTFILES_SECRET="$HOME/src/itisrazza/dotfiles/secret.git"
 
 alias dfgit="git --git-dir=\"${DOTFILES_COMMON}\" --work-tree=\"${HOME}\""
 alias dfpgit="git --git-dir=\"${DOTFILES_PLATFORM}\" --work-tree=\"${HOME}\""
